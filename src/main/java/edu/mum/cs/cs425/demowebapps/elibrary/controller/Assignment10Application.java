99d824a9af5786964c8115f8965b031924114f13
Lida <y_uguest@yahoo.co.uk>
2023-08-08 14:38:08 -0500

    initial commit

---
+++ src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/controller/Assignment10Application.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment10Application.class, args);
+    }
+
+}
---
+++ src/test/java/miu/edu/assignment10/Assignment10ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.assignment10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
